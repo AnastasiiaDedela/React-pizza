fb4794ceac1081273484f4ebb5c332e93d0db2d3
Anastasiia <nastyadedela@gmail.com>
2024-02-17 12:01:22 +0100

    feat: add pagination

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import './scss/app.scss';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
+
 import Header from './components/Header';
 import Home from './pages/Home';
 import Cart from './pages/Cart';
 import NotFound from './pages/NotFound';
-import { Routes, Route } from 'react-router-dom';
-import { useState } from 'react';
+
+import './scss/app.scss';
 
 function App() {
   const [searchValue, setSearchValue] = useState('');
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import Categories from '../components/Categories';
+import Pagination from '../components/Pagination';
 import Sort from '../components/Sort';
 import PizzaSkeleton from '../components/PizzaItem/PizzaSkeleton';
 import PizzaItem from '../components/PizzaItem';
@@ -10,6 +11,7 @@ const Home = ({ searchValue }) => {
   const [isLoading, setIsLoading] = useState(true);
   const [categoryId, setCategoryId] = useState(0);
   const [sortType, setSortType] = useState('rating');
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
     setIsLoading(true);
@@ -17,8 +19,10 @@ const Home = ({ searchValue }) => {
     const order = sortType.includes('-') ? 'desc' : 'asc';
     const sortBy = sortType.replace('-', '');
     const category = categoryId > 0 ? `category=${categoryId}` : '';
+    const search = searchValue ? `&search=${searchValue}` : '';
+
     fetch(
-      `https://65b69bf5da3a3c16ab00f9b4.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}`,
+      `https://65b69bf5da3a3c16ab00f9b4.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`,
     )
       .then((res) => res.json())
       .then((json) => {
@@ -26,17 +30,9 @@ const Home = ({ searchValue }) => {
         setIsLoading(false);
       });
     window.scrollTo(0, 0);
-  }, [categoryId, sortType]);
+  }, [categoryId, sortType, searchValue, currentPage]);
 
-  const pizzas = pizzaItems
-    .filter((obj) => {
-      if (obj.title.toLowerCase().includes(searchValue.toLowerCase())) {
-        return true;
-      }
-
-      return false;
-    })
-    .map((object) => <PizzaItem key={object.id} {...object} />);
+  const pizzas = pizzaItems.map((object) => <PizzaItem key={object.id} {...object} />);
   const skeletons = [...new Array(6)].map((_, index) => <PizzaSkeleton key={index} />);
 
   return (
@@ -47,6 +43,7 @@ const Home = ({ searchValue }) => {
       </div>
       <h2 className="content__title">All pizzas</h2>
       <div className="content__items">{isLoading ? skeletons : pizzas}</div>
+      <Pagination onChangePage={(number) => setCurrentPage(number)} />
     </div>
   );
 };
